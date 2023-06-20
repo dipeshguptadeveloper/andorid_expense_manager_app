c5beb7b1ddc70613a5381bdc520e489ca582209d
dipeshgupta <dipeshguptadeveloper@gmail.com>
2023-06-20 22:37:25 +0530

    add_expense activity added

--- app/src/main/java/com/dkgtech/expensemanager/screens/add_expense/AddExpenseActivity.kt
+++ app/src/main/java/com/dkgtech/expensemanager/screens/add_expense/AddExpenseActivity.kt
@@ -2,11 +2,33 @@ package com.dkgtech.expensemanager.screens.add_expense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.dkgtech.expensemanager.R
+import com.dkgtech.expensemanager.databinding.ActivityAddExpenseBinding
+import com.dkgtech.expensemanager.repo.ExpenseRepo
+import com.dkgtech.expensemanager.roomdb.AppDatabase
+import com.dkgtech.expensemanager.screens.add_expense.viewModel.AddExpenseViewModel
+import com.dkgtech.expensemanager.screens.add_expense.viewModel.AddExpenseViewModelFactory
+import com.dkgtech.expensemanager.screens.dashboard.viewModel.DashboardViewModel
+import com.dkgtech.expensemanager.screens.dashboard.viewModel.DashboardViewModelFactory
 
 class AddExpenseActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAddExpenseBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_expense)
+        binding = ActivityAddExpenseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val appDb = AppDatabase.getDatabase(this)
+
+        val expenseRepo = ExpenseRepo(appDb.ExpenseDao())
+
+        val addExpenseViewModel = ViewModelProvider(
+            this@AddExpenseActivity,
+            AddExpenseViewModelFactory(expenseRepo)
+        )[AddExpenseViewModel::class.java]
+
+
     }
 }
